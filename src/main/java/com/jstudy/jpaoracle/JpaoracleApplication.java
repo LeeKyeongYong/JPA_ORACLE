8d9df14d1d3cf65e03a897e4464574b118c69a4e
이경용 <sleekydz86@icloud.com>
2023-12-02 17:23:13 +0900

    jpa_oracle버전 셋팅
    jpa_oracle 버전 초기 셋팅

---
+++ src/main/java/com/jstudy/jpaoracle/JpaoracleApplication.java
@@ -0,0 +1,13 @@
+package com.jstudy.jpaoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaoracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaoracleApplication.class, args);
+	}
+
+}
